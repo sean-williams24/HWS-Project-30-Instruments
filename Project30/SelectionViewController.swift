4ce93d9f260d2785d7099d25191a650e2dca62b9
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-11-14 09:42:02 +0000

    FEAT: Fix shadow drawing bug by creating bezier path for CG path. Create rect of cell size to limit image size. Register cell class in view did load and dequeue reusable cells.

--- Project30/SelectionViewController.swift
+++ Project30/SelectionViewController.swift
@@ -17,6 +17,8 @@ class SelectionViewController: UITableViewController {
         super.viewDidLoad()
 
 		title = "Reactionist"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
 
 		tableView.rowHeight = 90
 		tableView.separatorStyle = .none
@@ -56,21 +58,22 @@ class SelectionViewController: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
 		// find the image for this cell, and load its thumbnail
 		let currentImage = items[indexPath.row % items.count]
 		let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
 		let path = Bundle.main.path(forResource: imageRootName, ofType: nil)!
 		let original = UIImage(contentsOfFile: path)!
-
-		let renderer = UIGraphicsImageRenderer(size: original.size)
+        let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
+        
+        let renderer = UIGraphicsImageRenderer(size: renderRect.size)
 
 		let rounded = renderer.image { ctx in
-			ctx.cgContext.addEllipse(in: CGRect(origin: CGPoint.zero, size: original.size))
+			ctx.cgContext.addEllipse(in: renderRect)
 			ctx.cgContext.clip()
 
-			original.draw(at: CGPoint.zero)
+			original.draw(in: renderRect)
 		}
 
 		cell.imageView?.image = rounded
@@ -80,6 +83,7 @@ class SelectionViewController: UITableViewController {
 		cell.imageView?.layer.shadowOpacity = 1
 		cell.imageView?.layer.shadowRadius = 10
 		cell.imageView?.layer.shadowOffset = CGSize.zero
+        cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: renderRect).cgPath
 
 		// each image stores how often it's been tapped
 		let defaults = UserDefaults.standard
