390ffd2f8e7b76e0e31cc308ed4ca86915fb1e3c
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-11-14 13:55:44 +0000

    FEAT: load images from file when app loads.

--- Project30/SelectionViewController.swift
+++ Project30/SelectionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SelectionViewController: UITableViewController {
 	var items = [String]() // this is the array that will store the filenames to load
+    var images = [UIImage]()
 	var dirty = false
 
     override func viewDidLoad() {
@@ -24,14 +25,22 @@ class SelectionViewController: UITableViewController {
 
 		// load all the JPEGs into our array
 		let fm = FileManager.default
-
-		if let tempItems = try? fm.contentsOfDirectory(atPath: Bundle.main.resourcePath!) {
-			for item in tempItems {
-				if item.range(of: "Large") != nil {
-					items.append(item)
-				}
-			}
-		}
+        if let tempItems = try? fm.contentsOfDirectory(atPath: Bundle.main.resourcePath!) {
+            for item in tempItems {
+                if item.range(of: "Large") != nil {
+                    items.append(item)
+                }
+            }
+        }
+        
+        for item in items {
+            let imageRootName = item.replacingOccurrences(of: "Large", with: "Thumb")
+            let path = Bundle.main.path(forResource: imageRootName, ofType: nil)
+            let image = UIImage(contentsOfFile: path!)
+            images.append(image!)
+            print(images.count)
+        }
+        
     }
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -42,6 +51,11 @@ class SelectionViewController: UITableViewController {
 			tableView.reloadData()
 		}
 	}
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 
     // MARK: - Table view data source
 
@@ -58,32 +72,30 @@ class SelectionViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-		// find the image for this cell, and load its thumbnail
+        
+        // find the image for this cell, and load its thumbnail
+        let original = images[indexPath.row % images.count]
+        // get string name of image for storing how many times its been tapped.
 		let currentImage = items[indexPath.row % items.count]
-		let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
-        if let path = Bundle.main.path(forResource: imageRootName, ofType: nil) {
-            let original = UIImage(contentsOfFile: path)
-            let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
-            
-            let renderer = UIGraphicsImageRenderer(size: renderRect.size)
 
-            let rounded = renderer.image { ctx in
-                ctx.cgContext.addEllipse(in: renderRect)
-                ctx.cgContext.clip()
+        let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
+        let renderer = UIGraphicsImageRenderer(size: renderRect.size)
 
-                original?.draw(in: renderRect)
-            }
-            
-            cell.imageView?.image = rounded
+        let rounded = renderer.image { ctx in
+            ctx.cgContext.addEllipse(in: renderRect)
+            ctx.cgContext.clip()
 
-            // give the images a nice shadow to make them look a bit more dramatic
-            cell.imageView?.layer.shadowColor = UIColor.black.cgColor
-            cell.imageView?.layer.shadowOpacity = 1
-            cell.imageView?.layer.shadowRadius = 10
-            cell.imageView?.layer.shadowOffset = CGSize.zero
-            cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: renderRect).cgPath
+            original.draw(in: renderRect)
         }
+        
+        cell.imageView?.image = rounded
+
+        // give the images a nice shadow to make them look a bit more dramatic
+        cell.imageView?.layer.shadowColor = UIColor.black.cgColor
+        cell.imageView?.layer.shadowOpacity = 1
+        cell.imageView?.layer.shadowRadius = 10
+        cell.imageView?.layer.shadowOffset = CGSize.zero
+        cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: renderRect).cgPath
 
 		// each image stores how often it's been tapped
 		let defaults = UserDefaults.standard
